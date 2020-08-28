966e68019cfae99341adef3e13289ad4a5dc8f00
Jason Gorman <jason.gorman@codemanship.com>
2020-08-28 09:47:23 +0100

    Initial commit

---
+++ LegacyCode/Pricer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TestPyramid
+{
+    internal class Pricer
+    {
+        public static Movie Price(string imdbId)
+        {
+            async Task<ImdbMovie> Json()
+            {
+                HttpResponseMessage response = await new HttpClient().GetAsync("http://www.omdbapi.com/?i=" + imdbId + "&apikey=6487ec62");
+
+                response.EnsureSuccessStatusCode();
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                return JsonSerializer.Deserialize<ImdbMovie>(json);
+            }
+
+
+            ImdbMovie imdbMovie = Json().Result;
+
+            double price = 3.95;
+
+            double rating = Double.Parse(imdbMovie.imdbRating);
+            
+            if(rating > 7.0)
+                price += 1.0;
+
+            if (rating < 4)
+                price -= 1.0;
+            
+            return new Movie(imdbMovie.Title, price);
+        }
+    }
+}
---
+++ Test.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using TestPyramid;
+
+namespace Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imdbID = args[0];
+            string member = args[1];
+            Rental rental = new Rental(imdbID, member);
+            System.Console.WriteLine("imdb ID: " + rental.ImdbID + ", member: " + rental.Member + ", title: " 
+                                     + rental.Title + ", price: " + rental.Price);
+        }
+    }
+}
